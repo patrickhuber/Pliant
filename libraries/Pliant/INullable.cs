b79afd3954eeba2a0249b48cb9b8f38d45935302
Patrick Huber <prhuber@gmail.com>
2015-04-04 16:32:13 -0400

    adds nullable interface

---
+++ libraries/Pliant/INullable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pliant
+{
+    public interface INullable<T>
+    {
+        T Value { get; }
+        bool HasValue { get; }
+    }
+}
