4ca5344d243f9f5700106336f492f4d803fffdba
patrick huber <prhuber@gmail.com>
2016-06-07 23:57:05 -0400

    adds namespace expression and tests

---
+++ tests/Pliant.Tests.Unit/Builders/Expressions/NamespaceExpressionTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pliant.Builders.Expressions;
+using Pliant.Builders.Models;
+
+namespace Pliant.Tests.Unit.Builders.Expressions
+{
+    [TestClass]
+    public class NamespaceExpressionTests
+    {
+        [TestMethod]
+        public void NamespaceExpressionShouldCreateFullyQualifiedNameFromNamespaceExpressionAndNameString()
+        {
+            NamespaceExpression ns1 = "namespace1";
+            ProductionExpression
+                S = ns1 + "S",
+                A = ns1 + "A";
+            S.Rule = A;
+            A.Rule = 'a';
+
+            var symbolS = S.ProductionModel.LeftHandSide;
+            Assert.IsNotNull(symbolS);
+            Assert.AreEqual(ns1.Namespace, symbolS.Value.Namespace);
+            Assert.AreEqual(S.ProductionModel.LeftHandSide.Value.Name, symbolS.Value.Name);
+
+            var symbolA = A.ProductionModel.LeftHandSide;
+            Assert.IsNotNull(symbolA);
+            Assert.AreEqual(ns1.Namespace, symbolA.Value.Namespace);
+            Assert.AreEqual(A.ProductionModel.LeftHandSide.Value.Name, symbolA.Value.Name);
+        }
+    }
+}
