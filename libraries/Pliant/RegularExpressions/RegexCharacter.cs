9d2a0587dfa3f5ac61b95e2c5208025767d13841
patrick huber <prhuber@gmail.com>
2017-05-09 11:34:29 -0400

    updates regex to recognize escaped character classes.

--- libraries/Pliant/RegularExpressions/RegexCharacter.cs
+++ libraries/Pliant/RegularExpressions/RegexCharacter.cs
@@ -5,22 +5,25 @@ namespace Pliant.RegularExpressions
 {
     public class RegexCharacter : RegexNode
     {
-        public char Value { get; set; }
+        private readonly int _hashCode;
+
+        public char Value { get; private set; }
+
+        public bool IsEscaped { get; private set; }
 
         public override RegexNodeType NodeType
         {
-            get { return RegexNodeType.RegexCharacter; }
+            get { return RegexNodeType.RegexCharacterClassCharacter; }
         }
 
-        public RegexCharacter(char value)
+        public RegexCharacter(char value, bool isEscaped = false)
         {
             Value = value;
+            IsEscaped = isEscaped;
             _hashCode = ComputeHashCode();
         }
-        
-        private readonly int _hashCode;
 
-        int ComputeHashCode()
+        private int ComputeHashCode()
         {
             return HashCode.Compute(
                     Value.GetHashCode());
@@ -30,19 +33,22 @@ namespace Pliant.RegularExpressions
         {
             return _hashCode;
         }
-        
+
         public override bool Equals(object obj)
         {
             if ((object)obj == null)
                 return false;
-            var character = obj as RegexCharacter;
-            if ((object)character == null)
+            var regexCharacter = obj as RegexCharacter;
+            if ((object)regexCharacter == null)
                 return false;
-            return character.Value.Equals(Value);
+            return regexCharacter.Value.Equals(Value) 
+                && regexCharacter.IsEscaped.Equals(IsEscaped);
         }
 
         public override string ToString()
         {
+            if (IsEscaped)
+                return $"\\{Value}";
             return Value.ToString();
         }
     }
