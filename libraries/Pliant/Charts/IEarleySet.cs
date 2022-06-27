99ad68d9b256d8b0d766ee717e943a9ec7581854
patrickhuber <patrickhuber@users.noreply.github.com>
2022-06-27 11:32:22 -0400

    adds transition caching

--- libraries/Pliant/Charts/IEarleySet.cs
+++ libraries/Pliant/Charts/IEarleySet.cs
@@ -17,8 +17,25 @@ namespace Pliant.Charts
 
         int Location { get; }
 
+        /// <summary>
+        /// Returns the transition state with the same symbol as the search symbol
+        /// </summary>
+        /// <param name="searchSymbol"></param>
+        /// <returns></returns>
         ITransitionState FindTransitionState(ISymbol searchSymbol);
-        
-        IState FindSourceState(ISymbol searchSymbol);
+
+        /// <summary>
+        /// Returns all predictions where the postdot symbol is the same as the search symbol
+        /// </summary>
+        /// <param name="searchSymbol"></param>
+        /// <returns></returns>
+        IReadOnlyList<INormalState> FindSourceStates(ISymbol searchSymbol);
+
+        /// <summary>
+        /// Returns all completions where the left hand symbol is the same as the search symbol
+        /// </summary>
+        /// <param name="symbol"></param>
+        /// <returns></returns>
+        IReadOnlyList<INormalState> FindReductions(ISymbol symbol);
     }
 }
